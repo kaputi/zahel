abb8402e1acee02f0b91954de801154050a676a6
Eduardo Lonighi <edu_loni3@hotmail.com>
2024-03-29 10:10:08 +0000

    BubbleSort

---
+++ src/__tests__/BubbleSort.ts
@@ -0,0 +1,7 @@
+import BubbleSort from '@code/invested/sorting/BubbleSort';
+
+test('BubbleSort', () => {
+  const arr = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+  BubbleSort(arr);
+  expect(arr).toEqual([1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9]);
+});
